a27e31fc6fb28d2a1d22b0e5aca4a308f7bc35dc
Júlio Cezar Mendo <mendo.julio.c@gmail.com>
2023-01-19 16:06:47 -0300

    testing the sorter with linkedlist

--- build/index.js
+++ build/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const BetterSorter_1 = require("./BetterSorter");
 const NumbersCollection_1 = require("./NumbersCollection");
 const CharactersCollection_1 = require("./CharactersCollection");
+const LinkedLists_1 = require("./LinkedLists");
 class Sorter {
     // using unions is a bad idea, as usual, because
     // we have to keep adding if statements to fix
@@ -43,3 +44,11 @@ const charactersCollection = new CharactersCollection_1.CharactersCollection('Xa
 const sortedChar = new BetterSorter_1.BetterSorter(charactersCollection);
 sortedChar.sort();
 console.log(numbersCollection.data, charactersCollection.data);
+const linkedList = new LinkedLists_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const sortedList = new BetterSorter_1.BetterSorter(linkedList);
+sortedList.sort();
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { BetterSorter } from './BetterSorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedLists';
 class Sorter {
   // using unions is a bad idea, as usual, because
   // we have to keep adding if statements to fix
@@ -42,3 +43,13 @@ const charactersCollection = new CharactersCollection('Xaayb');
 const sortedChar = new BetterSorter(charactersCollection);
 sortedChar.sort();
 console.log(numbersCollection.data, charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sortedList = new BetterSorter(linkedList);
+sortedList.sort();
+linkedList.print();
